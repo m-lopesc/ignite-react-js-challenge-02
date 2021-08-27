0cb42e4e2f6b67038be188f1fd007a1c8bc8c5bd
Mateus Castro <mlopesc3@icloud.com>
2021-08-27 00:30:20 -0300

    prettier

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,16 +4,16 @@ import '../styles/button.scss';
 import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  title: string;
-  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  selected: boolean;
+	title: string;
+	iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+	selected: boolean;
 }
 
 export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
-  return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
-      {title}
-    </button>
-  );
+	return (
+		<button type='button' {...(selected && { className: 'selected' })} {...rest}>
+			<Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+			{title}
+		</button>
+	);
 }
